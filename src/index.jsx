a5a58f5e6c86a1102fe9ba9dc25400962c02dd66
magicq6265@gmail.com <62584142+jadenHong@users.noreply.github.com>
2020-10-26 19:01:39 -0600

    country-app

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import promise from 'redux-promise-middleware';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger'
+import promise from 'redux-promise-middleware';
 import rootReducer from './reducers';
 import { App } from './App';
 import './styles/index.scss';
